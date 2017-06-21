0fbf4e02b853688a130f4073e8d826ca2a56af3e
Emily Ringoen <emilyaringoen@gmail.com>
2017-06-21 16:17:19 -0600

    fixing a few files

--- backend/app.js
+++ backend/app.js
@@ -16,13 +16,11 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', index)
-app.use('/api/users', users)
 
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(express.static(path.join(__dirname, '/../', 'node_modules')))
+
+app.use('/api/users', users)
 
 app.use('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
